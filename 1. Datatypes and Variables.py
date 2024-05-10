8762480a5f4c857b1f3ea84e7b9a68b24f531142
Sanjana2308 <81796362+Sanjana2308@users.noreply.github.com>
2024-05-10 14:30:15 +0530

    Add files via upload

---
+++ 1. Datatypes and Variables.py
@@ -0,0 +1,20 @@
+# Different Datatypes in Python
+string = "My name is Sanjana" #String
+num = 1 #Integer
+flt = 25.63 #Float
+boo = True #Boolean
+
+
+print("My name is Sanjana")
+print("I am an engineer")
+print("I am from India")
+
+# Using Variables
+name = "Sanjana"
+occ = "engineer"
+nal = "India"
+
+print("My name is "+name)
+print("I am an "+occ)
+print("I am from "+nal)
+
